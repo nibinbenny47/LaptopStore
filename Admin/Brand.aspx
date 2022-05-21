6fb7084a78990a7cd37ce9425ea4c9a1108260de
Nibin Benny <nibinbenny47@gmail.com>
2022-05-21 08:56:08 +0530

    brand name adding

---
+++ Admin/Brand.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Brand.aspx.cs" Inherits="LaptopStore.Admin.Brand" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblname" runat="server" Text="Enter name"></asp:Label>
+            <asp:TextBox runat="server" ID="txtname"></asp:TextBox><br />
+            <asp:Button ID="btnsave" runat="server" Text="save" OnClick="btnsave_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Admin/Brand.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Web;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace LaptopStore.Admin
+{
+    public partial class Brand : System.Web.UI.Page
+    {
+        string connectionstring = WebConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
